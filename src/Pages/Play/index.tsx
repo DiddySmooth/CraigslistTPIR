dc715eceefcd26dcf8a39a276385dc337d523e2f
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2022-02-13 14:10:33 -0600

    added placeholder shit

--- src/Pages/Play/index.tsx
+++ src/Pages/Play/index.tsx
@@ -1,7 +1,43 @@
 const Play = () => {
     return(
         <div>
-            Play the game bitch
+            <div>
+                <h1>Guess the Price</h1>
+            </div>
+            <div>
+                <h1>Rolex Yacht Master II Ceramic 44 mm 116680 All Boxes & Papers.MINT</h1>
+                <img src="https://images.craigslist.org/00Z0Z_3D9fAD3njd2z_0x20m1_600x450.jpg"/>
+                <p>Up for sale is This sought after all stainless 116680 Rolex Yachtmaster II is in very crispy hardly worn condition. This model incorporates the "blue hands" and have been discontinued by Rolex and are no longer available making this watch highly desirable to collectors and enthusiasts alike. The case and bracelet are free of indentations, pivot , dings etc truly immaculate. This watch comes complete with box, papers, and warranty card.
+                    Reference Number 116680
+                    Manufacturer Rolex YACHT-MASTER II
+                    Age Warranty Card December 2016
+                    Box YES
+                    Papers YES
+                    Gender Men's / Unisex
+                    Movement Automatic
+                    Caliber 4161
+                    Power Reserve 72 hour
+                    Number of Jewels 42
+                    Case Material Stainless Steel
+                    Case Size 44 mm
+                    Water Resistance 10 ATM
+                    Bezel Ceramic Blue
+                    Crystal Sapphire
+                    Dial White
+                    Dial Numerals None / Luminous Markers
+                    Bracelet Material Stainless Steel
+                    Clasp Folding Oysterlock
+                    Watch Category High Grade
+                    Condition Pre-Owned
+                    The watch is in an impressive MINT condition and fully link . Please serious inquires only .Thanks for looking
+                </p>
+            </div>
+            <div>
+                <form>
+                    <input placeholder="Guess to nearest dollar" type="number"/>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     )
 }
