63bbcbc8e9098833e7c619599d643d46e01dcdd2
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2022-02-13 13:27:42 -0600

    first changes to react and added bootstrap and react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="light">
+                <Nav.Link href="home">Home</Nav.Link>
+        </Navbar>
+        <Home />
     </div>
   );
 }
