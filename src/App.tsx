3baaa38d02708fa3ca1ebd54aebbf11c3d3ff8bd
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2022-02-14 19:08:46 -0600

    added userlist

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,19 @@ import io from 'socket.io-client';
 function App() {
     const [socket, setSocket] = useState<any>();
     const [message, setMessage] = useState("Hello")
-    const connect = () => {
-        console.log(message)
-        if(socket){
-            socket.emit("sendMessage", message)
-        }
-    }
-    if(socket){
-        socket.on("serverMessage", (serverMessage:any) => {
-            console.log(serverMessage)
-        })
-    }
+    
     useEffect(() => {
         const newSocket: any = io(`${process.env.REACT_APP_BACKEND}`);
         setSocket(newSocket);
         return () => newSocket.close();
     }, [])
-
+    console.log(socket)
     return (
         <div className="App">
-            <button onClick={connect}>Connect</button>
             <NavBar />
             <Routes>
                 <Route path="/home" element={<Home />}/>
-                <Route path="/play" element={<Play />}/>
+                <Route path="/play" element={<Play socket={socket}/>}/>
             </Routes>
         </div>
   );
--- src/Pages/Play/index.tsx
+++ src/Pages/Play/index.tsx
@@ -1,13 +1,15 @@
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import posts from '../../Assets/dummyData'
-const Play = () => {
+const Play = ({socket}:any) => {
+
+    const [username, setUsername] = useState("")
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [img, setImg] = useState([""])
     const [price, setPrice] = useState<number>()
     const [guess, setGuess] = useState<number>()
+    const [users, setUsers] = useState<any>([])
     useEffect(() => {
-        console.log("Hello")
         setTitle(posts[0].title)
         setDescription(posts[0].description)
         setImg(posts[0].pictures)
@@ -17,11 +19,32 @@ const Play = () => {
 
     const submitGuess = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        if(price && guess)
-        console.log(`your guess was off by ${price - guess}`)
     }
+
+    const login = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if(socket){
+            socket.emit("login", username)
+        }
+
+    }
+    console.log(socket)
+    if(socket){
+        socket.on("serverMessage", (serverMessage:any) => {
+            console.log(serverMessage)
+            
+            setUsers(serverMessage)
+        })
+    }
+    console.log(users)
     return(
         <div>
+            <div>
+                <form onSubmit={login}>
+                    <input placeholder='Please Enter a Name' value={username} onChange={(e) => {setUsername(e.target.value)}}/>
+                    <input type="submit" />
+                </form>
+            </div>
             <div>
                 <h1>Guess the Price</h1>
             </div>
@@ -35,6 +58,14 @@ const Play = () => {
                     <input placeholder="Guess to nearest dollar" type="number" value={guess} onChange={(e) => {setGuess(parseInt(e.target.value))}}/>
                     <input type="submit" />
                 </form>
+            </div>
+                {users && (
+                    users.map((user:any) => 
+                        <li>{user}</li>
+                    )
+                )}
+            <div>
+                
             </div>
         </div>
     )
